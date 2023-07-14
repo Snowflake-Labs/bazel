a702b9db06ee9c6f42a91e27a413469adcd95dcf
Julio Merino <julio.merino+oss@snowflake.com>
2023-07-14 10:32:41 -0700

    Add the experimental_use_remote_cache_for_cache_unaware_spawns flag
    This new flag is similar in spirit to --remote_accept_cached but allows
    being more selective about what's accepted and what's not.
    
    The specific problem I face is the following: we have a setup where we
    want to use dynamic execution for performance reasons.  However, we know
    some actions in our build (those run by rules_foreign_cc) are not
    deterministic.  To mitigate this, we force the actions that we know are
    not deterministic to run remotely, without dynamic execution, as this
    will prevent exposing the non-determinism for as long as they are cached
    and until we can fix their problems.
    
    However, we still observe non-deterministic actions in the build and we
    need to diagnose what those are.  To do this, I need to run two builds
    and compare their execlogs.  And I need these builds to continue to
    reuse the non-deterministic artifacts we _already_ know about from the
    cache, but to rerun other local actions from scratch.
    
    Unfortunately, the fact that "remote-cache" is not a strategy (see
    https://github.com/bazelbuild/bazel/issues/18245) makes this very
    difficult to do because, even if I configure certain actions to run
    locally unconditionally, the spawn strategy insists on checking the
    remote cache for them.
    
    With this new flag, I can run a build where the remote actions remain
    remote but where I disable the dynamic scheduler and force the remaining
    actions to re-run locally.  I'm marking the flag as experimental because
    this feels like a huge kludge to paper over the fact that the remote
    cache should really be a strategy, but isn't.  In other words: this
    flag should go away with a better rearchitecting of the remote caching
    interface.
    
    Upstream PR: https://github.com/bazelbuild/bazel/pull/18944

--- src/main/java/com/google/devtools/build/lib/sandbox/SandboxModule.java
+++ src/main/java/com/google/devtools/build/lib/sandbox/SandboxModule.java
@@ -425,6 +425,7 @@ public final class SandboxModule extends BlazeModule {
     return new SandboxFallbackSpawnRunner(
         sandboxSpawnRunner,
         createFallbackRunner(env),
+        env.getOptions().getOptions(ExecutionOptions.class),
         env.getReporter(),
         sandboxOptions != null && sandboxOptions.legacyLocalFallback);
   }
@@ -434,6 +435,7 @@ public final class SandboxModule extends BlazeModule {
         env.getOptions().getOptions(LocalExecutionOptions.class);
     return new LocalSpawnRunner(
         env.getExecRoot(),
+        env.getOptions().getOptions(ExecutionOptions.class),
         localExecutionOptions,
         env.getLocalResourceManager(),
         LocalEnvProvider.forCurrentOs(env.getClientEnv()),
@@ -455,16 +457,19 @@ public final class SandboxModule extends BlazeModule {
     private final ExtendedEventHandler reporter;
     private static final AtomicBoolean warningEmitted = new AtomicBoolean();
     private final boolean fallbackAllowed;
+    private final boolean handlesCaching;
 
     SandboxFallbackSpawnRunner(
         SpawnRunner sandboxSpawnRunner,
         SpawnRunner fallbackSpawnRunner,
+        ExecutionOptions executionOptions,
         ExtendedEventHandler reporter,
         boolean fallbackAllowed) {
       this.sandboxSpawnRunner = sandboxSpawnRunner;
       this.fallbackSpawnRunner = fallbackSpawnRunner;
       this.reporter = reporter;
       this.fallbackAllowed = fallbackAllowed;
+      this.handlesCaching = !executionOptions.useRemoteCacheForCacheUnawareSpawns;
     }
 
     @Override
@@ -510,7 +515,7 @@ public final class SandboxModule extends BlazeModule {
 
     @Override
     public boolean handlesCaching() {
-      return false;
+      return handlesCaching;
     }
 
     @Override
--- src/test/java/com/google/devtools/build/lib/worker/WorkerSpawnRunnerTest.java
+++ src/test/java/com/google/devtools/build/lib/worker/WorkerSpawnRunnerTest.java
@@ -51,6 +51,7 @@ import com.google.devtools.build.lib.collect.nestedset.NestedSet;
 import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
 import com.google.devtools.build.lib.collect.nestedset.Order;
 import com.google.devtools.build.lib.events.ExtendedEventHandler;
+import com.google.devtools.build.lib.exec.ExecutionOptions;
 import com.google.devtools.build.lib.exec.SpawnExecutingEvent;
 import com.google.devtools.build.lib.exec.SpawnRunner.SpawnExecutionContext;
 import com.google.devtools.build.lib.exec.local.LocalEnvProvider;
@@ -154,6 +155,7 @@ public class WorkerSpawnRunnerTest {
             /* binTools= */ null,
             resourceManager,
             /* runfilesTreeUpdater= */ null,
+            new ExecutionOptions(),
             new WorkerOptions(),
             metricsCollector,
             new JavaClock());
@@ -545,6 +547,7 @@ public class WorkerSpawnRunnerTest {
         /* binTools= */ null,
         resourceManager,
         /* runfilesTreeUpdater= */ null,
+        new ExecutionOptions(),
         workerOptions,
         metricsCollector,
         new JavaClock());
