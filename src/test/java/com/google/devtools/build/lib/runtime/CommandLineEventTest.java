5f143eda4e9c3b7f4b26039ed1a34642101146b4
Julio Merino <julio.merino+oss@snowflake.com>
2023-10-05 10:34:10 -0700

    Sanitize BEP contents before emitting them
    This change adds logic to clean up the contents of the BEP before it
    leaves Bazel in an attempt to prevent propagating secrets to a remote
    service.
    
    In particular, this implements protections for the following:
    
    *   Secrets stored in the environment which are unconditionally leaked
        via the hidden --client_env flag.
    
    *   Secrets passed via --bes_header, --test_env, --test_arg, and similar
        options.  This is not completely safe to do because a determined
        user could inject secrets into the BEP in other ways.  So... treat
        this as just a safety net for common innocent mistakes.
    
    *   Secrets passed to tools run via "bazel run".  Think, for example,
        doing "bazel run //some/docker:command -- --password 1234".
    
    For the purposes of this change, a "secret" is whatever string we did
    not explicitly allow.  This code does not implement secret detection
    and it does not indend to, because such code would be based on
    heuristics as well.
    
    Note that services like BuildBuddy implement their own redaction of
    secrets when they receive the BEP... but it's better if we can strip
    them out at the source to minimize exposure.  Defense in depth.

--- src/test/java/com/google/devtools/build/lib/runtime/CommandLineEventTest.java
+++ src/test/java/com/google/devtools/build/lib/runtime/CommandLineEventTest.java
@@ -572,4 +572,68 @@ public class CommandLineEventTest {
     assertThat(line.getSections(4).getChunkList().getChunk(0)).isEqualTo("//some:target");
     assertThat(line.getSections(4).getChunkList().getChunk(1)).isEqualTo("REDACTED");
   }
+
+@Test
+  public void testScrubEnvOption() throws OptionsParsingException {
+    OptionsParser fakeStartupOptions =
+        OptionsParser.builder().optionsClasses(BlazeServerStartupOptions.class).build();
+    OptionsParser fakeCommandOptions =
+        OptionsParser.builder().optionsClasses(TestOptions.class).build();
+    fakeCommandOptions.parse(
+        PriorityCategory.COMMAND_LINE,
+        "command line",
+        ImmutableList.of(
+            "--stub_env=HOME=/home/jmmv",
+            "--stub_env=NOT_ALLOWED=1234",
+            "--stub_env", "ANOTHER=foo=1234",
+            "--stub_inherit_env=HOME",
+            "--stub_inherit_env=NOT_ALLOWED"
+        ));
+
+    CommandLine line =
+        new OriginalCommandLineEvent(
+            "testblaze",
+            fakeStartupOptions,
+            "someCommandName",
+            fakeCommandOptions,
+            Optional.of(ImmutableList.of()))
+            .asStreamProto(null)
+            .getStructuredCommandLine();
+
+    assertThat(line.getCommandLineLabel()).isEqualTo("original");
+    checkCommandLineSectionLabels(line);
+
+    assertThat(line.getSections(0).getChunkList().getChunk(0)).isEqualTo("testblaze");
+    assertThat(line.getSections(1).getOptionList().getOptionCount()).isEqualTo(0);
+    assertThat(line.getSections(2).getChunkList().getChunk(0)).isEqualTo("someCommandName");
+    // Expect the rc file options and invocation policy options to not be listed with the explicit
+    // command line options.
+    assertThat(line.getSections(3).getOptionList().getOptionCount()).isEqualTo(5);
+    {
+      Option option = line.getSections(3).getOptionList().getOption(0);
+      assertThat(option.getCombinedForm()).isEqualTo("--stub_env=HOME=/home/jmmv");
+      assertThat(option.getOptionValue()).isEqualTo("HOME=/home/jmmv");
+    }
+    {
+      Option option = line.getSections(3).getOptionList().getOption(1);
+      assertThat(option.getCombinedForm()).isEqualTo("--stub_env=NOT_ALLOWED= ");
+      assertThat(option.getOptionValue()).isEqualTo("NOT_ALLOWED= ");
+    }
+    {
+      Option option = line.getSections(3).getOptionList().getOption(2);
+      assertThat(option.getCombinedForm()).isEqualTo("--stub_env ANOTHER= ");
+      assertThat(option.getOptionValue()).isEqualTo("ANOTHER= ");
+    }
+    {
+      Option option = line.getSections(3).getOptionList().getOption(3);
+      assertThat(option.getCombinedForm()).isEqualTo("--stub_inherit_env=HOME");
+      assertThat(option.getOptionValue()).isEqualTo("HOME");
+    }
+    {
+      Option option = line.getSections(3).getOptionList().getOption(4);
+      assertThat(option.getCombinedForm()).isEqualTo("--stub_inherit_env=NOT_ALLOWED");
+      assertThat(option.getOptionValue()).isEqualTo("NOT_ALLOWED");
+    }
+    assertThat(line.getSections(4).getChunkList().getChunkCount()).isEqualTo(0);
+  }
 }
